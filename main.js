81b83b0139e5c141aefc7435191de3c577511317
UwUontheweb <94400578+UWUontheweb@users.noreply.github.com>
2021-11-30 11:00:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+var score = 0;
+var omgb = 0;
+var cursurcosttauto = 100;
+var omg = 0
+
+  
+
+var cursurcost = 15;
+var Cursurs = 0;
+var cursorcosturbo = 1500;
+var omgc = 0;
+
+function buycursor (amount) {
+  if (score >= cursurcost) {
+    score = score - cursurcost;
+          
+    omg = omg + 1;
+    cursurcost = cursurcost + 7;
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("cursurcost").innerHTML = cursurcost;
+    document.getElementById("cursurs").innerHTML = cursurs;
+
+      }
+  }
+
+
+  function BuyTurboclick(amount) {
+    if (score >= cursorcosturbo) {
+      score = score - cursorcosturbo;
+
+      omgc = omgc + 1  
+      cursorcosturbo = cursorcosturbo + 17;
+
+      document.getElementById("score").innerHTML = score;
+      document.getElementById("cursorcostturbo").innerHTML = cursorcosturbo;
+      }
+  }
+
+  function buyAutoclick(amount) {
+    if (score >= cursurcosttauto) {
+      score = score - cursurcosttauto;
+
+      omgb = omgb + 1
+
+      cursurcosttauto = cursurcosttauto + 13
+         
+      document.getElementById("score").innerHTML = score;
+      document.getElementById("cursurCostauto").innerHTML = cursurcosttauto;
+          
+
+      }
+  }
+
+  function AddToScore(amount){
+    score=score + amount;
+    document.getElementById("score").innerHTML = score;
+  }
+  
+  setInterval (function () {
+    score = score + omg;
+    document.getElementById("score").innerHTML = score;
+      
+      
+  }, 1000);
+
+  setInterval (function () {
+      
+    score = score + omgb;
+
+    document.getElementById("score").innerHTML = score;
+      
+      
+  }, 300);
